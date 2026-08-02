aed9b049d0185ccbcf638419179ddda8ed972de8
fredrickclassic5-crypto <fredrickclassic5@gmail.com>
2026-08-02 09:58:00 -0700

    Replace Font link and restructure contact section in index.html; update form submission logic in script.js

--- script.js
+++ script.js
@@ -15,25 +15,7 @@ if(top>=offset && top<offset+height){
 section.classList.add("active");
 
 }
-<script>
-const form = document.getElementById("contact-form");
 
-form.addEventListener("submit", function(e){
-    e.preventDefault();
-
-    emailjs.sendForm(
-        "ntd2c0b",
-        "11btROuuOHVTVw_uolthV",
-        this
-    ).then(() => {
-        alert("Message sent successfully!");
-        form.reset();
-    }, (error) => {
-        alert("Failed to send message.");
-        console.log(error);
-    });
-});
-</script>
 });
 
 });
